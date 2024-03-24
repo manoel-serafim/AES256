07667b6e568068bcc168fff82c37eb90dfc9a648
Manoel Serafim <manoel.serafim@proton.me>
2024-03-24 18:16:47 -0300

    <feat> (cbc) ::mode logic is finished, it was actually fairly simple

--- src/mode/cbc.c
+++ src/mode/cbc.c
@@ -1,9 +1,35 @@
 #include "mode/cbc.h"
+#include "encryption.h"
 
-void cbc(BYTE key[32], BYTE block[16]){
+
+/*
+    [Cipher Block Chaining Mode]
+    @param input length :: length in bytes 
+*/
+
+void cbc(BYTE key[32], BYTE* input, size_t length){
+
     BYTE initialization_vector[16];
     init_iv(initialization_vector);
 
-    (WORD*)block ^= (WORD*)initialization_vector[0] 
+    ((WORD*)input)[0] ^= ((WORD*)initialization_vector)[0];
+    ((WORD*)input)[1] ^= ((WORD*)initialization_vector)[1];
+    ((WORD*)input)[2] ^= ((WORD*)initialization_vector)[2];
+    ((WORD*)input)[3] ^= ((WORD*)initialization_vector)[3];
+
+    encrypt(input, key);
+
+    for(;length>0; length-=16)
+    {
+        input += 16;
+        ((WORD*)input)[0] ^= ((WORD*)input)[-4];
+        ((WORD*)input)[1] ^= ((WORD*)input)[-3];
+        ((WORD*)input)[2] ^= ((WORD*)input)[-2];
+        ((WORD*)input)[3] ^= ((WORD*)input)[-1];
+
+        encrypt(input, key);
+    }
+        
+
 }
 
